19c9d52053d6fce60f27ed05b59d874d2127ec93
AnastasiaGainulina <109802756+AnastasiaGainulina@users.noreply.github.com>
2022-10-02 22:49:35 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 64: Задайте значения M и N. 
+// Напишите программу, которая выведет все 
+// натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+string NumbersFor(int M, int N)
+{
+    string result = String.Empty;
+    for (int i=M; i<=N; i++)
+    {
+        result += $"{i} ";
+    }
+    return result;
+}
+
+string NumbersRec(int M, int N)
+{
+    if (M<=N) return $"{M} " +NumbersRec(M+1, N);
+    else return String.Empty;
+}
+System.Console.WriteLine(NumbersFor(4,8));
+System.Console.WriteLine(NumbersRec(4,8));
